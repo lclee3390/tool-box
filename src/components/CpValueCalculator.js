d268f37016026bf62183213c9fbef4a8b5d883ba
lclee <lclee3390@gmail.com>
2024-08-24 17:40:33 +0800

    multi unit

--- src/components/CpValueCalculator.js
+++ src/components/CpValueCalculator.js
@@ -1,22 +1,36 @@
 // src/components/CpValueCalculator.js
 import React, { useState, useEffect } from "react";
-
 function CpValueCalculator({ showPage }) {
   const [items, setItems] = useState([
-    { id: 1, volume: "", price: "", note: "", unitPrice: null },
+    {
+      id: 1,
+      units: [{ value: "" }],
+      price: "",
+      note: "",
+      unitPrice: null,
+    },
   ]);
   const [nextId, setNextId] = useState(2); // 用來追踪下一個商品的ID
   const [highlightedItems, setHighlightedItems] = useState([]);
   const [errors, setErrors] = useState({});
 
   useEffect(() => {
-    // 在網頁載入後自動加入兩個項目
-    addItem();
+    if (items.length === 1) {
+      // 確保只在初次載入時添加
+      addItem();
+    }
   }, []);
+
   const addItem = () => {
     setItems([
       ...items,
-      { id: nextId, volume: "", price: "", note: "", unitPrice: null },
+      {
+        id: nextId,
+        units: [{ value: "" }],
+        price: "",
+        note: "",
+        unitPrice: null,
+      },
     ]);
     setNextId(nextId + 1); // 更新下一個ID
   };
@@ -28,6 +42,22 @@ function CpValueCalculator({ showPage }) {
     setHighlightedItems([]); // 清除高亮显示
   };
 
+  const handleUnitChange = (id, index, value) => {
+    setItems(
+      items.map((item) =>
+        item.id === id
+          ? {
+              ...item,
+              units: item.units.map((unit, unitIndex) =>
+                unitIndex === index ? { ...unit, value } : unit
+              ),
+            }
+          : item
+      )
+    );
+    setHighlightedItems([]);
+  };
+
   const deleteItem = (id) => {
     setItems(items.filter((item) => item.id !== id));
     setHighlightedItems(
@@ -36,6 +66,29 @@ function CpValueCalculator({ showPage }) {
     const newErrors = { ...errors };
     delete newErrors[id];
     setErrors(newErrors);
+  };
+
+  const addUnit = (id) => {
+    setItems(
+      items.map((item) =>
+        item.id === id
+          ? { ...item, units: [...item.units, { value: "" }] }
+          : item
+      )
+    );
+  };
+
+  const removeUnit = (id, index) => {
+    setItems(
+      items.map((item) =>
+        item.id === id
+          ? {
+              ...item,
+              units: item.units.filter((_, unitIndex) => unitIndex !== index),
+            }
+          : item
+      )
+    );
   };
 
   const validateItems = () => {
@@ -52,14 +105,17 @@ function CpValueCalculator({ showPage }) {
         itemErrors.price = "請輸入有效的價格";
         isValid = false;
       }
-      if (
-        item.volume === "" ||
-        isNaN(parseFloat(item.volume)) ||
-        parseFloat(item.volume) <= 0
-      ) {
-        itemErrors.volume = "請輸入有效的容量";
-        isValid = false;
-      }
+      item.units.forEach((unit, index) => {
+        if (
+          unit.value === "" ||
+          isNaN(parseFloat(unit.value)) ||
+          parseFloat(unit.value) <= 0
+        ) {
+          unit.value = 1
+          // itemErrors[`unit${index + 1}`] = `請輸入有效的單位${index + 1}`;
+          // isValid = false;
+        }
+      });
       if (Object.keys(itemErrors).length > 0) {
         newErrors[item.id] = itemErrors;
       }
@@ -78,11 +134,16 @@ function CpValueCalculator({ showPage }) {
     let highlightedItemsTemp = [];
 
     const updatedItems = items.map((item) => {
-      const volumeNum = parseFloat(item.volume);
+      // 計算 volumeNum，處理空值或0值
+      let volumeNum = item.units.reduce((acc, unit) => {
+        const unitValue = parseFloat(unit.value);
+        return acc * (isNaN(unitValue) || unitValue === 0 ? 1 : unitValue);
+      }, 1);
+
       const priceNum = parseFloat(item.price);
       let unitPrice = null;
 
-      if (!isNaN(volumeNum) && !isNaN(priceNum) && volumeNum > 0) {
+      if (!isNaN(priceNum) && volumeNum > 0) {
         unitPrice = priceNum / volumeNum;
 
         if (unitPrice < lowestUnitPrice) {
@@ -98,15 +159,31 @@ function CpValueCalculator({ showPage }) {
 
     setItems(updatedItems);
     setHighlightedItems(highlightedItemsTemp);
+
+    console.log("highlightedItemsTemp:", highlightedItemsTemp); // 檢查計算結果
   };
+
+  const sanitizeCSVValue = (value) => {
+    return value.replace(/"/g, '""'); // 避免 CSV 格式問題
+  };
+
   const exportToCSV = () => {
     const csvRows = [
-      ["ID", "備註", "價格", "容量", "單位價格"],
+      [
+        "ID",
+        "備註",
+        "價格",
+        ...Array.from(
+          { length: items[0].units.length },
+          (_, i) => `單位${i + 1}`
+        ),
+        "單位價格",
+      ],
       ...items.map((item) => [
         item.id,
-        item.note,
+        sanitizeCSVValue(item.note),
         item.price,
-        item.volume,
+        ...item.units.map((unit) => unit.value),
         item.unitPrice !== null ? item.unitPrice.toFixed(2) : "",
       ]),
     ];
@@ -123,26 +200,33 @@ function CpValueCalculator({ showPage }) {
     link.click();
     document.body.removeChild(link);
   };
+
   const clearAllItems = () => {
-    setItems([{ id: 1, volume: "", price: "", note: "", unitPrice: null }]);
+    setItems([
+      {
+        id: 1,
+        units: [{ value: "" }],
+        price: "",
+        note: "",
+        unitPrice: null,
+      },
+    ]);
     setNextId(2); // 重置ID計數器
     setHighlightedItems([]); // 清除高亮顯示
     setErrors({}); // 清除錯誤訊息
   };
+
   return (
     <div id="cp-value-calculator" style={styles.container}>
       <div style={styles.header}>
-        <button style={styles.addButton} onClick={addItem}>
-          + 添加
-        </button>
-        
-        <h2 style={styles.title}></h2>
-        
-        <button style={styles.exportButton} onClick={exportToCSV}>
-            匯出
-        </button>
+
+        {/* <h2 style={styles.title}>計算機</h2> */}
+
+        {/* <button style={styles.exportButton} onClick={exportToCSV}>
+          匯出
+        </button> */}
         <button style={styles.clearButton} onClick={clearAllItems}>
-            清除
+          清除
         </button>
       </div>
       {items.map((item, index) => (
@@ -175,9 +259,6 @@ function CpValueCalculator({ showPage }) {
             >
               刪除
             </button>
-           
-
-            
           </div>
           <div style={styles.fields}>
             <div style={styles.inputGroup}>
@@ -194,23 +275,36 @@ function CpValueCalculator({ showPage }) {
                 }
                 style={styles.input}
               />
-              
             </div>
-            <div style={styles.inputGroup}>
+            <div style={styles.unitTitle}>
               <span style={styles.inputLabel}>單位</span>
-              {errors[item.id]?.volume && (
-                <div style={styles.error}>{errors[item.id].volume}</div>
-              )}
-              <input
-                type="number"
-                placeholder="容量"
-                value={item.volume}
-                onChange={(e) =>
-                  handleInputChange(item.id, "volume", e.target.value)
-                }
-                style={styles.input}
-              />
+              <button style={styles.addUnitButton} onClick={() => addUnit(item.id)}>
+                + 添加單位
+              </button>
             </div>
+            {item.units.map((unit, unitIndex) => (
+              <div style={styles.inputUnitGroup} key={unitIndex}>
+                {/* <span style={styles.inputLabel}>單位{unitIndex + 1}</span> */}
+                <input
+                  type="number"
+                  placeholder={`單位${unitIndex + 1}`}
+                  value={unit.value}
+                  onChange={(e) =>
+                    handleUnitChange(item.id, unitIndex, e.target.value)
+                  }
+                  style={styles.input}
+                />
+                {unitIndex > 0 && (
+                  <button
+                    style={styles.deleteButton}
+                    onClick={() => removeUnit(item.id, unitIndex)}
+                  >
+                    X
+                  </button>
+                )}
+              </div>
+            ))}
+            
           </div>
           {item.unitPrice !== null && (
             <div style={styles.calculation}>
@@ -219,6 +313,10 @@ function CpValueCalculator({ showPage }) {
           )}
         </div>
       ))}
+      
+      <button style={styles.addButton} onClick={addItem}>
+          + 添加商品項目
+        </button>
       <button style={styles.calcButton} onClick={calculateBestValue}>
         計算最佳 CP值
       </button>
@@ -230,174 +328,198 @@ function CpValueCalculator({ showPage }) {
     </div>
   );
 }
+
 const styles = {
-    container: {
-      padding: "20px",
-      maxWidth: "100%",
-      margin: "0 auto",
-      backgroundColor: "#f9f9f9",
-      borderRadius: "8px",
-      boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
-    },
-    header: {
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        marginBottom: "20px",
-        flexWrap: "wrap",
-      },
-    title: {
-      textAlign: "center",
-      margin: "10px 0",
-      color: "#333",
-      flexGrow: 1,
-    },
-    item: {
-      marginBottom: "15px",
-      padding: "15px",
-      borderRadius: "8px",
-      border: "2px solid #ccc",
-      boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+  container: {
+    padding: "20px",
+    maxWidth: "100%",
+    margin: "0 auto",
+    backgroundColor: "#f9f9f9",
+    borderRadius: "8px",
+    boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
+  },
+  header: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: "20px",
+    flexWrap: "wrap",
+  },
+  title: {
+    textAlign: "center",
+    margin: "10px 0",
+    color: "#333",
+    flexGrow: 1,
+  },
+  item: {
+    marginBottom: "15px",
+    padding: "15px",
+    borderRadius: "8px",
+    border: "2px solid #ccc",
+    boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+  },
+  fields: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: "5px",
+    gap: "3px",
+    flexWrap: "wrap",
+  },
+  fieldsName: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: "5px",
+    gap: "3px",
+    flexWrap: "nowrap",
+  },
+  itemLabel: {
+    fontWeight: "bold",
+    color: "#4CAF50",
+  },
+  noteInput: {
+    flex: "1 1 auto",
+    padding: "8px",
+    borderRadius: "4px",
+    border: "1px solid #ccc",
+    boxSizing: "border-box",
+    textAlign: "left",
+  },
+  inputGroup: {
+    display: "flex",
+    alignItems: "center",
+    gap: "5px",
+    flex: "1 1 100%",
+    flexDirection: "row",
+    position: "relative",
+    flexWrap: "wrap",
+  },
+  inputUnitGroup: {
+    display: "flex",
+    alignItems: "center",
+    gap: "5px",
+    flex: "1 1 100%",
+    flexDirection: "row",
+    // position: "relative",
+    flexWrap: "nowrap",
+  },
+  inputLabel: {
+    flexShrink: 0,
+    color: "#555",
+  },
+  input: {
+    flex: "1 1 100%",
+    padding: "8px",
+    borderRadius: "4px",
+    border: "1px solid #ccc",
+    boxSizing: "border-box",
+  },
+  error: {
+    color: "#ff0000",
+    fontSize: "0.8em",
+  },
+  deleteButton: {
+    backgroundColor: "#FF5722",
+    color: "white",
+    padding: "8px",
+    border: "none",
+    borderRadius: "4px",
+    cursor: "pointer",
+    flexShrink: 0,
+    marginLeft: "10px",
+  },
+  calculation: {
+    marginTop: "10px",
+    fontSize: "1em",
+    color: "#333",
+    textAlign: "right",
+    fontWeight: "bold",
+  },
+  addButton: {
+    backgroundColor: "#4CAF50",
+    color: "white",
+    padding: "10px",
+    border: "none",
+    borderRadius: "5px",
+    cursor: "pointer",
+    fontSize: "1em",
+    flexShrink: 0,
+    marginBottom: "10px",
+  },
+
+  unitTitle:{
+    marginTop:"5px",
+
+  },
+  addUnitButton: {
+    marginLeft:"5px",
+    backgroundColor: "#4CAF50",
+    color: "white",
+    // padding: "10px",
+    border: "none",
+    borderRadius: "5px",
+    cursor: "pointer",
+    fontSize: "1em",
+    flexShrink: 0,
+    // marginBottom: "10px",
+  },
+  calcButton: {
+    backgroundColor: "#4CAF50",
+    color: "white",
+    padding: "10px 20px",
+    border: "none",
+    borderRadius: "5px",
+    cursor: "pointer",
+    fontSize: "1em",
+    width: "100%",
+    marginBottom: "20px",
+  },
+  backLink: {
+    textDecoration: "none",
+    color: "#4CAF50",
+    fontWeight: "bold",
+    textAlign: "center",
+    display: "block",
+    marginTop: "20px",
+  },
+  clearButton: {
+    backgroundColor: "#F44336",
+    color: "white",
+    padding: "10px",
+    border: "none",
+    borderRadius: "5px",
+    cursor: "pointer",
+    fontSize: "1em",
+    flexShrink: 0,
+    marginLeft: "auto",
+  },
+  exportButton: {
+    backgroundColor: "#2196F3",
+    color: "white",
+    padding: "10px",
+    border: "none",
+    borderRadius: "5px",
+    cursor: "pointer",
+    fontSize: "1em",
+    flexShrink: 0,
+    marginRight: "5px",
+    marginLeft: "auto",
+  },
+  "@media (max-width: 768px)": {
+    inputGroup: {
+      flexDirection: "column",
     },
     fields: {
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "space-between",
-      marginBottom: "5px",
-      gap: "3px",
-      flexWrap: "wrap",
-    },
-    fieldsName: {
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "space-between", // 保持刪除按鈕在最右側
-        marginBottom: "5px",
-        gap: "3px",
-        flexWrap: "nowrap", // 防止換行
-      },
-    itemLabel: {
-      fontWeight: "bold",
-      color: "#4CAF50",
-    //   marginRight: "10px",
-    },
-    noteInput: {
-        flex: "1 1 auto",
-        padding: "8px",
-        borderRadius: "4px",
-        border: "1px solid #ccc",
-        boxSizing: "border-box",
-        textAlign: "left", // 確保文字靠左對齊
-      },
-    inputGroup: {
-      display: "flex",
-      alignItems: "center",
-      gap: "5px",
-      flex: "1 1 100%",
-      flexDirection: "row",
-      position: "relative",
-      flexWrap: "wrap",
-    },
-    inputLabel: {
-      flexShrink: 0,
-      color: "#555",
+      flexDirection: "column",
     },
     input: {
-      flex: "1 1 100%", // 在小螢幕上讓 input 填滿可用空間
-      padding: "8px",
-      borderRadius: "4px",
-      border: "1px solid #ccc",
-      boxSizing: "border-box",
-    },
-    error: {
-      color: "#ff0000",
-      fontSize: "0.8em",
-    },
-    deleteButton: {
-        backgroundColor: "#FF5722",
-        color: "white",
-        padding: "8px",
-        border: "none",
-        borderRadius: "4px",
-        cursor: "pointer",
-        flexShrink: 0,
-        marginLeft: "10px", // 給刪除按鈕增加一點間距
-      },
-    calculation: {
-      marginTop: "10px",
-      fontSize: "1em",
-      color: "#333",
-      textAlign: "right",
-      fontWeight: "bold",
-    },
-    addButton: {
-      backgroundColor: "#4CAF50",
-      color: "white",
-      padding: "10px",
-      border: "none",
-      borderRadius: "5px",
-      cursor: "pointer",
-      fontSize: "1em",
-      flexShrink: 0,
-      marginBottom: "10px",
-    },
-    calcButton: {
-      backgroundColor: "#4CAF50",
-      color: "white",
-      padding: "10px 20px",
-      border: "none",
-      borderRadius: "5px",
-      cursor: "pointer",
-      fontSize: "1em",
       width: "100%",
-      marginBottom: "20px",
     },
-    backLink: {
-      textDecoration: "none",
-      color: "#4CAF50",
-      fontWeight: "bold",
-      textAlign: "center",
-      display: "block",
-      marginTop: "20px",
+    noteInput: {
+      width: "100%",
     },
-    "@media (max-width: 768px)": {
-      inputGroup: {
-        flexDirection: "column", // 垂直排列
-      },
-      fields: {
-        flexDirection: "column", // 垂直排列
-      },
-      input: {
-        width: "100%", // 確保在小螢幕上 input 填滿容器
-      },
-      noteInput: {
-        width: "100%", // 確保在小螢幕上 note input 填滿容器
-      },
-    },
-    clearButton: {
-        backgroundColor: "#F44336",
-        color: "white",
-        padding: "10px",
-        border: "none",
-        borderRadius: "5px",
-        cursor: "pointer",
-        fontSize: "1em",
-        flexShrink: 0,
-        marginLeft: "auto", // 將按鈕移到最右側
-      },
-      exportButton: {
-        backgroundColor: "#2196F3",
-        color: "white",
-        padding: "10px",
-        border: "none",
-        borderRadius: "5px",
-        cursor: "pointer",
-        fontSize: "1em",
-        flexShrink: 0,
-        marginRight: "5px",
-        marginLeft: "auto", // 將按鈕移到最右側
+  },
+};
 
-      },
-  };
-  
-  export default CpValueCalculator;
-  
+export default CpValueCalculator;
